04cbdac3b896996623e0379758aa4b7d363be3cb
Miguel-David Orrico Teruel <otm@iesseveroochoa.net>
2018-11-23 15:52:28 +0100

    Clases de prueba de los ejercicios del examen. PDF con el enunciado del examen

---
+++ src/PruebaEjercicios4.java
@@ -0,0 +1,18 @@
+public class PruebaEjercicios4 {
+    public static void main(String[] args) {
+        /* Descomentar una vez resueltos
+        System.out.println(Enmascara.enmascara("4556364607935616"));
+        System.out.println(Enmascara.enmascara("64607935616"));
+        System.out.println(Enmascara.enmascara("1"));
+        System.out.println(Enmascara.enmascara(""));
+
+        // "¿Cuál era el nombre de tu primera mascota?"
+        System.out.println(Enmascara.enmascara("Skippy"));
+        System.out.println(Enmascara.enmascara("Nanananananananananananananananananananan Batman!"));
+
+        System.out.println(ADN.devuelveComplementario("ATTGC"));
+
+        System.out.println(ADN.devuelveComplementario("GTAT"));
+         */
+    }
+}
---
+++ src/PruebaPais.java
@@ -0,0 +1,16 @@
+public class PruebaPais {
+    public static void main(String[] args) {
+        /*Descomentar para probar una vez resuelto
+        PaisEuropeo espanya=new PaisEuropeo("España",1000);
+        PaisEuropeo reinoUnido=new PaisEuropeo("Reino Unido",3000,"uk");
+
+        espanya.aportar(3000);
+        reinoUnido.aportar(5000);
+        System.out.println("Hay " + PaisEuropeo.getFondoComun() + " en el fondo común");
+        espanya.retirar(9000);
+        System.out.println("Hay " + PaisEuropeo.getFondoComun() + " en el fondo común");
+        System.out.println(espanya.getNombre() + " es de catagoría " + espanya.getCategoria());
+
+         */
+    }
+}
